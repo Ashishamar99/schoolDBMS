7d2bd46388f81820169efe79726deb6a1fe1ce23
Ashish Amar <ashishamar1999@gmail.com>
2021-09-14 19:07:04 +0530

    web hook test 4

--- springbootdemo/src/main/java/com/brillio/springbootdemo/Class2.java
+++ springbootdemo/src/main/java/com/brillio/springbootdemo/Class2.java
@@ -2,6 +2,6 @@ package com.brillio.springbootdemo;
 
 public class Class2 {
     public static void main(String[] args) {
-        System.out.println("Testing Web hook demo 3");
+        System.out.println("Testing Web hook demo 4");
     }
 }
